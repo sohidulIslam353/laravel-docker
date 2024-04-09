dd163ec029a80abd56c8625d4a26df24b5de5074
sohidulislam353 <sohidulislam353@gmail.com>
2024-04-09 16:27:14 +0600

    add redis mailpit etc

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -90,7 +90,7 @@
                                         </div>
 
                                         <div class="pt-3 sm:pt-5 lg:pt-0">
-                                            <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
+                                            <h2 class="text-xl font-semibold text-black dark:text-white">Documentation Github</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
                                                 Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
